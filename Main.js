39cce408d60b40dcb012a6a3ca6577569dd135e3
ascha10 <ascha9957@gmail.com>
2021-08-05 01:08:23 +0300

    first commit

---
+++ Main.js
@@ -0,0 +1,303 @@
+/*************************firstQuestion**********************************/
+
+
+// var numberOne = Number(prompt("Please Enter The First Number"));
+// var numberTwo = Number(prompt("Please Enter The Second Number"));
+
+// console.log({numberOne},{numberTwo});
+
+
+// if (numberOne + numberTwo >= 13) {
+
+//     console.log("bar-mitzva");
+
+// }else if (numberOne + numberTwo == 12) {
+
+//     console.log("bat-mitzva");
+
+// }else{
+
+//     console.log("kid");
+
+// }
+
+
+/*************************SecondQuestionPartOne**************************/
+
+// var N = 0;
+
+// while (N < 100) {
+    
+//     console.log("Yes");
+//     N++;
+// }
+
+/****************************SecondQuestionPartTwo**********************/
+
+// var B = 0;
+// var userInput = Number(prompt("Please Enter A Number"));
+
+// while ( B < userInput ) {
+    
+//     console.log("No");
+
+//     B++;
+// }
+
+
+/****************************THhirdQuestion**********************/
+
+// for (var x = 0; x < 1000 ; x++ ) {
+
+//     console.log("JavaScript Is Not Java");
+
+// }
+
+
+/****************************FourthQuestion**********************/
+
+
+// var userInputName = prompt("Please Enter A Name");
+// var userInputNumber = prompt("Please Enter A Number");
+
+// for (var i = 0; i < userInputNumber ; i++ ) {
+
+//     console.log(userInputName);
+
+// }
+
+/****************************FifthQuestion**********************/
+
+
+// var userInputName = prompt("Please Enter A Name");
+// var userInputAge = prompt("Please Enter Age");
+// var userInputEmail = prompt("Please Enter A Email");
+
+// console.log({userInputName} ,{userInputAge},{userInputEmail}  );
+
+// var Info = ` ${userInputName} ${userInputAge} ${userInputEmail} `
+
+// var userInputValue = prompt("Please Enter A Value");
+
+// for (var z = 0; z < userInputValue ; z++ ) {
+
+//     if (userInputAge > 18) {
+
+//         console.log({Info});
+
+//     }       
+// }
+
+/****************************SixthQuestion**********************/
+
+// var userInputNumber = Number (prompt("Please Enter A Number"));
+// var userInputName = prompt("Please Enter A Name");
+
+// var myName = "BoB";
+
+// for (var a = 0; a < userInputNumber ; a++  ) {
+
+//     userInputName = prompt("Please Enter A Name");
+
+//     if (userInputName == myName) {
+
+//         console.log("Hi Boss BoB" );
+
+//     }else{
+
+//         console.log("No Entry");
+
+//     }
+    
+// }
+
+
+/****************************SeventhQuestion**********************/
+
+// var userInputFirstNumber = Number (prompt("Please Enter The First Number "));
+// var userInputSecondNumber = Number (prompt("Please Enter The Second Number"));
+
+
+// for (var i = 0 ; i < userInputFirstNumber ; i++ ) {
+
+
+//     for (var j = 0; j < userInputSecondNumber ; j++) {
+
+//         console.log( `i * j = ${ i * j } `);
+
+//     }
+
+// }
+
+
+
+/****************************Eight + NineQuestion**********************/
+
+// var myArray = [12, 14 ,78 ,77,99];
+
+// console.log(myArray);
+
+
+// var myNameArray = ["sid","sasi" ,"mary" ,"david" ,"alon" ];
+
+// for (var i = 0; i < myNameArray.length ; i++) {
+
+//     console.log( myNameArray [i] );
+
+// }
+
+
+/****************************TenQuestion**********************/
+
+
+// var myArray = [12, 14 ,78 ,77,99, 45 , 10 , 20 , 30 , 40];
+
+// for (var G = 0; G < myArray.length ; G++) {
+
+//     if ( myArray[G]%2 == 0 ) {
+
+//         console.log(myArray[G]);
+
+//     }
+
+// }
+
+
+/****************************ElevenQuestion**********************/
+
+// var myArrayName = [];
+// var userInput = prompt("Please Enter A Name")
+
+// for ( var e = 0 ; e < 5 ; e++ ) {
+
+//     myArrayName[e] = userInput = prompt("Please Enter A Name")
+//     console.log(myArrayName[e]);
+
+// }
+
+// console.log(myArrayName);
+
+
+
+/****************************TwelveQuestion**********************/
+
+
+// var myArrayNumber = [];
+// var userInput = Number (prompt("Please Enter A Number "));
+
+// for (var i = 0; i < 7 ; i++ ) {
+
+//     userInputNumber = Number (prompt("Please Enter A Number"));
+
+//     if (userInputNumber%2 == 1) {
+
+//         myArrayNumber[i] = userInputNumber = Number (prompt("Please Enter A Number"));
+
+//         console.log(myArrayNumber[i]);
+//     }
+// }
+
+// console.log(myArrayNumber);
+
+
+/****************************Thirteenuestion**********************/
+
+
+// var myArray = ["dali", 78, "arik",12, "vova" ,20, "david",30, "bob" ,14 ,"ford",45 ];
+
+
+// for (var f = 0; f < myArray.length; f++ ) {
+
+//     if (myArray[f]*0 != 0 ) {
+
+//         myArray[f] = "not_number"
+//     }
+
+// }
+
+// console.log(myArray);
+
+
+/****************************Fourteenuestion**********************/
+
+// var myArrayNumber = [];
+// var userInputNumber = Number (prompt("Please Enter A Number"));
+// var times = 0;
+
+// while (times < 10) {
+
+//     userInputNumber = Number (prompt("Please Enter A Number"));
+
+//     if (userInputNumber%2 == 0 ) {
+
+//         myArrayNumber[times] = userInputNumber 
+//         console.log(userInputNumber[times]);
+//         times++
+//     }
+
+    
+// }
+
+// console.log(myArrayNumber);
+
+
+
+/****************************SixteenuestionPartOne**********************/
+
+
+// var userInputWidth = Number (prompt ("Please Enter A Value For Width"))
+// var userInputHeight = Number (prompt ("Please Enter A Value For Height"))
+
+// var rectanglePerimeter = userInputWidth * 2 + userInputHeight *2;
+
+// console.log({rectanglePerimeter});
+
+
+/****************************SixteenQuestionPartTwo**********************/
+
+// var firstInput = Number (prompt ("Please Enter The First Number"))
+// var secondInput = Number (prompt ("Please Enter THe Second Number"))
+// var thirdInput = Number (prompt ("Please Enter The Third Number"))
+
+
+// if (firstInput > secondInput && secondInput > thirdInput ) {
+
+//     console.log("Decending Order");
+    
+// }else if (firstInput < secondInput && secondInput < thirdInput  ) {
+    
+//     console.log("Ascending Order");
+    
+// }else{
+
+//     console.log("There Is No Order");
+
+// }
+
+
+/****************************EightteenQuestion**********************/
+
+
+// var number = +prompt("enter a number");
+// var numArray = [];
+// for(var i=0; i < number.toString().length;i++){
+//     if(number > 0){
+//         numArray[i] = number.toString().charAt(i);
+//         console.log(numArray[i]);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
